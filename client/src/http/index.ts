3414be3a03654542e287e66eb5bdcfc22b12e81b
Roshan Kumar <rsahu7989@gmail.com>
2024-07-19 23:22:50 +0530

    Added API client with axios

---
+++ client/src/http/index.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import config from "../configs/config";
+const api = axios.create({
+    baseURL: config.getAPIURL(),
+    withCredentials: true,
+    headers: {
+        "Content-Type": 'application/json',
+        Accept: 'application/json',
+    }
+});
+
+interface SignupUserData {
+    name: string;
+    email: string;
+    password: string;
+}
+
+interface LoginUserData {
+    email: string;
+    password: string;
+}
+
+export const login = async (data: LoginUserData) => {
+    return api.post('/auth/login', data);
+};
+
+export const signup = async (data: SignupUserData) => {
+    return api.post('/auth/signup', data);
+};
