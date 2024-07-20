9a63f436c9884cc8c4b12e0f0024049cb41ccccf
Roshan Kumar <rsahu7989@gmail.com>
2024-07-20 17:04:38 +0530

    Add refreshAccessToken Route

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,5 +1,5 @@
 import UserModel from '../models/user.model';
-import { User } from 'user';
+import { User, FilterQuery } from 'user';
 
 class UserService {
 
@@ -19,6 +19,21 @@ class UserService {
     async getUserByEmail(email: string): Promise<User | null> {
         try {
             const user = await UserModel.findOne({ email }).exec();
+            if (!user) {
+                console.log('User not found');
+                return null;
+            }
+            return user;
+        } catch (error) {
+            console.error('Error retrieving user:', error);
+            throw new Error('Failed to retrieve user');
+        }
+    }
+
+
+    async getUser(filter: FilterQuery): Promise<User | null> {
+        try {
+            const user = await UserModel.findOne(filter).exec();
             if (!user) {
                 console.log('User not found');
                 return null;
