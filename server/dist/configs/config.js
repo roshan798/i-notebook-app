9989a74864be39ba52a9c4e944ab6b405460908b
Roshan Kumar <rsahu7989@gmail.com>
2024-07-23 00:30:39 +0530

    'stage'

---
+++ server/dist/configs/config.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const config = {
+    server: {
+        port: process.env.PORT || 8000,
+        host: process.env.HOST || 'localhost',
+    },
+    client: {
+        url: process.env.CLIENT_URL || 'http://localhost:5173',
+    },
+    database: {
+        uri: process.env.DATABASE_URL || '  mongodb://localhost:27017/i-notebook-app',
+    },
+    jwt: {
+        access_secret: process.env.ACCESS_JWT_SECRET || "a_secret",
+        refresh_secret: process.env.REFRESH_JWT_SECRET || "r_secret"
+    },
+};
+exports.default = config;
---
+++ server/dist/controllers/notes.controller.js
@@ -0,0 +1,82 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const validation_1 = require("../utils/validation");
+const notes_service_1 = __importDefault(require("../services/notes.service"));
+const notes_dto_1 = __importDefault(require("../DTO/notes.dto"));
+class NotesController {
+    createNote(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const noteData = req.body;
+            const notesValidationError = (0, validation_1.validateNote)(noteData);
+            if (notesValidationError !== null) {
+                return res.status(400)
+                    .json({
+                    success: false,
+                    message: notesValidationError
+                });
+            }
+            try {
+                const savedNote = yield notes_service_1.default.create(Object.assign(Object.assign({}, noteData), { userId: req.user._id }));
+                res.status(201).json({
+                    success: true,
+                    message: 'Note created successfully',
+                    note: new notes_dto_1.default(savedNote)
+                });
+            }
+            catch (error) {
+                console.log("Error creating note", error);
+                res.status(500).json({
+                    success: false,
+                    message: 'Internal server error'
+                });
+            }
+        });
+    }
+    getNotes(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const user = req.user;
+            try {
+                const notes = yield notes_service_1.default.getNotesByUserId(user._id);
+                res.json({
+                    success: true,
+                    notes: notes.map(note => new notes_dto_1.default(note))
+                });
+            }
+            catch (error) {
+                console.log("Error fetching notes", error);
+                res.status(500).json({
+                    success: false,
+                    message: 'Internal server error'
+                });
+            }
+        });
+    }
+    getNoteById(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            res.json({ message: 'GET Note by ID' });
+        });
+    }
+    updateNotes(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            res.json({ message: 'PUT Notes' });
+        });
+    }
+    deleteNotes(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            res.json({ message: 'DELETE Notes' });
+        });
+    }
+}
+exports.default = new NotesController;
